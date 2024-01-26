ae5031f3e80063b862a539472269268a096936bf
hwlee9905 <9905hyun@naver.com>
2024-01-26 17:38:38 +0900

    study commit

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,19 +1,27 @@
 package hello.login;
 
+import hello.login.web.argumentresolver.LogimMemberArgumentResolver;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.intercepter.LogInterceptor;
 import hello.login.web.intercepter.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LogimMemberArgumentResolver());
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
